ff29346edbec85edc4b93437724b3d3f8206a54b
leena <lenam4545m@gmail.com>
2025-09-01 04:05:23 +0300

    Initial Commit

---
+++ AtSight Watch App/AtSightApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtSightApp.swift
+//  AtSight Watch App
+//
+//  Created by Leena on 01/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct AtSight_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AtSight/AtSightApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtSightApp.swift
+//  AtSight
+//
+//  Created by Leena on 01/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct AtSightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
